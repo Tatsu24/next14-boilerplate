1af17b2f1a95782598729c97ea96184b1b86b7c6
Tatsuya Oyama <t_oyama@yumemi.co.jp>
2023-12-02 14:05:13 +0900

    chore: 不要なファイル削除

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,4 @@
-import { Inter } from 'next/font/google';
-
 import type { Metadata } from 'next';
-import './globals.css';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -12,8 +7,8 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='en'>
-      <body className={inter.className}>{children}</body>
+    <html lang='ja'>
+      <body>{children}</body>
     </html>
   );
 }
